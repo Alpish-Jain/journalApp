52878ba66b9e30d39b1d841e8ef371425c7d4cc2
Alpish-Jain <46166671+Alpish-Jain@users.noreply.github.com>
2025-01-22 09:08:00 +0530

    Adding Changes for AppCache

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -1,7 +1,10 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import net.engineeringdigest.journalApp.cache.AppCache;
+import net.engineeringdigest.journalApp.constants.Placeholders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +15,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-    private static final String apiKey="2c121a00aa518370c03efb4cc7cf8fad";
+    @Value("${weather.api.key}")
+    public String apiKey;
 
-    private static final String API="http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+    @Autowired
+    private AppCache appCache;
 
     @Autowired
     RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city){
-    String finalAPI=API.replace("CITY",city).replace("API_KEY",apiKey);
+    String finalAPI=appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY,city).replace(Placeholders.API_KEY,apiKey);
         ResponseEntity<WeatherResponse> response=restTemplate.exchange(finalAPI, HttpMethod.GET,null, WeatherResponse.class);
         WeatherResponse body=response.getBody();
         return body;
